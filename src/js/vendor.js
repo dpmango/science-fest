b2ea0f1e6e76dcc6de91a76d65f517094e61f3e8
Sergey Khmelevskoy <xs290@me.com>
2021-09-22 21:21:30 +0300

    init tpl

---
+++ src/js/vendor.js
@@ -0,0 +1,73 @@
+// import $ from 'jquery';
+import svg4everybody from 'svg4everybody';
+import picturefill from 'picturefill';
+import viewportUnitsBuggyfill from 'viewport-units-buggyfill';
+import objectFitImages from 'object-fit-images/dist/ofi.es-modules.js';
+import MicroModal from 'micromodal';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import AOS from 'aos';
+import validate from 'jquery-validation';
+import mask from 'jquery-mask-plugin';
+import Choices from 'choices.js';
+import Barba from 'barba.js';
+import Lazy from 'jquery-lazy';
+// import LazyAV from 'jquery-lazy/plugins/jquery.lazy.av.min.js';
+// import LazyPicture from 'jquery-lazy/plugins/jquery.lazy.picture.min.js';
+import { TweenMax, TimelineMax, Power2 } from 'gsap/all';
+import ScrollToPlugin from 'gsap/ScrollToPlugin';
+import debounce from 'lodash/debounce';
+import throttle from 'lodash/throttle';
+import { disablePageScroll, enablePageScroll, clearQueueScrollLocks } from 'scroll-lock';
+
+// uncomment plugins you want to use (i.e. from /js/__extras folder)
+
+// import scrollMonitor from 'scrollmonitor';
+// import sharer from 'sharer.js';
+// import ClipboardJS from 'clipboard';
+// import PhotoSwipe from 'photoswipe';
+// // eslint-disable-next-line camelcase
+// import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
+import datepicker from 'air-datepicker';
+import datepickerEn from 'air-datepicker/dist/js/i18n/datepicker.en.js';
+// import autofillEvent from 'autofill-event';
+// import magnificPopup from 'magnific-popup';
+// import selectric from 'jquery-selectric';
+
+// expose imports to window to use in app.js
+// (jquery is exposed in expose-loader)
+// window.jQuery = $;
+// window.$ = $;
+window.svg4everybody = svg4everybody;
+window.picturefill = picturefill;
+window.objectFitImages = objectFitImages;
+window.viewportUnitsBuggyfill = viewportUnitsBuggyfill;
+Swiper.use([Navigation, Pagination]);
+window.Swiper = Swiper;
+window.MicroModal = MicroModal;
+window.AOS = AOS;
+window.validate = validate;
+window.mask = mask;
+window.Choices = Choices;
+window.Barba = Barba;
+window.Lazy = Lazy;
+window.ScrollToPlugin = ScrollToPlugin;
+window.TweenMax = TweenMax;
+// window.TimelineMax = TimelineMax;
+window.debounce = debounce;
+window.throttle = throttle;
+window.disablePageScroll = disablePageScroll;
+window.enablePageScroll = enablePageScroll;
+window.clearQueueScrollLocks = clearQueueScrollLocks;
+
+// expose commented plugins
+
+// window.scrollMonitor = scrollMonitor;
+// window.sharer = sharer;
+// window.ClipboardJS = ClipboardJS;
+// window.PhotoSwipe = PhotoSwipe;
+// // eslint-disable-next-line camelcase
+// window.PhotoSwipeUI_Default = PhotoSwipeUI_Default;
+window.datepicker = datepicker;
+// window.autofillEvent = autofillEvent;
+// window.magnificPopup = magnificPopup;
+// window.selectric = selectric;
