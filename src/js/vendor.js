2f83a8ab72ae268639c220eefa65af735be9124e
Sergey Khmelevskoy <xs290@me.com>
2021-09-28 19:01:49 +0300

    events page

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -9,7 +9,6 @@ import AOS from 'aos';
 import validate from 'jquery-validation';
 import mask from 'jquery-mask-plugin';
 import Choices from 'choices.js';
-import Barba from 'barba.js';
 import Lazy from 'jquery-lazy';
 // import LazyAV from 'jquery-lazy/plugins/jquery.lazy.av.min.js';
 // import LazyPicture from 'jquery-lazy/plugins/jquery.lazy.picture.min.js';
@@ -20,18 +19,9 @@ import throttle from 'lodash/throttle';
 import { disablePageScroll, enablePageScroll, clearQueueScrollLocks } from 'scroll-lock';
 
 // uncomment plugins you want to use (i.e. from /js/__extras folder)
-
-// import scrollMonitor from 'scrollmonitor';
-// import sharer from 'sharer.js';
-// import ClipboardJS from 'clipboard';
-// import PhotoSwipe from 'photoswipe';
-// // eslint-disable-next-line camelcase
-// import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
 import datepicker from 'air-datepicker';
 import datepickerEn from 'air-datepicker/dist/js/i18n/datepicker.en.js';
 // import autofillEvent from 'autofill-event';
-// import magnificPopup from 'magnific-popup';
-// import selectric from 'jquery-selectric';
 
 // expose imports to window to use in app.js
 // (jquery is exposed in expose-loader)
@@ -48,7 +38,6 @@ window.AOS = AOS;
 window.validate = validate;
 window.mask = mask;
 window.Choices = Choices;
-window.Barba = Barba;
 window.Lazy = Lazy;
 window.ScrollToPlugin = ScrollToPlugin;
 window.TweenMax = TweenMax;
@@ -60,14 +49,6 @@ window.enablePageScroll = enablePageScroll;
 window.clearQueueScrollLocks = clearQueueScrollLocks;
 
 // expose commented plugins
-
-// window.scrollMonitor = scrollMonitor;
-// window.sharer = sharer;
-// window.ClipboardJS = ClipboardJS;
-// window.PhotoSwipe = PhotoSwipe;
-// // eslint-disable-next-line camelcase
-// window.PhotoSwipeUI_Default = PhotoSwipeUI_Default;
 window.datepicker = datepicker;
 // window.autofillEvent = autofillEvent;
-// window.magnificPopup = magnificPopup;
 // window.selectric = selectric;
